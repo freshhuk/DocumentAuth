e79874dd46c28301f2a523617c1d65127f3dca29
freshhuk <freshhuk13@gmail.com>
2024-11-01 15:41:39 +0200

    Initial commit

---
+++ src/main/java/com/document/documentauth/Configuration/RabbitmqConfig.java
@@ -0,0 +1,2 @@
+package com.document.documentauth.Configuration;public class RabbitmqConfig {
+}
---
+++ src/test/java/com/document/documentauth/DocumentAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document.documentauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
